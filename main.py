92b59832ac5548743236b87429b8ff83f22e41e2
Jeronimo Guerrero <jerogm08@gmail.com>
2026-03-31 11:12:25 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+#Programa principal
+name_surname = input("Enter your name and surname: ")
+age = int(input("Enter your age: "))
+city_residence = input("Enter your city of residence: ")
+print(f"The user {name_surname} is {age} years old and lives in {city_residence}.")
